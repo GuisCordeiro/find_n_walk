6cf174c3d253c90b602b9f20ae36852555c661cb
Guilherme Cordeiro <guilhermedossantoscordeiro13@gmail.com>
2021-10-13 15:55:42 -0300

    Começei a trabalhar na barra de pesquisa

--- findnwalk/lib/pages/homePage.dart
+++ findnwalk/lib/pages/homePage.dart
@@ -77,3 +77,32 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+
+/*TypeAheadField(
+  textFieldConfiguration: TextFieldConfiguration(
+    autofocus: true,
+    style: DefaultTextStyle.of(context).style.copyWith(
+      fontStyle: FontStyle.italic
+    ),
+    decoration: InputDecoration(
+      border: OutlineInputBorder()
+    )
+  ),
+  suggestionsCallback: (pattern) async {
+    return await BackendService.getSuggestions(pattern);
+  },
+  itemBuilder: (context, suggestion) {
+    return ListTile(
+      leading: Icon(Icons.shopping_cart),
+      title: Text(suggestion['name']),
+      subtitle: Text('\$${suggestion['price']}'),
+    );
+  },
+  onSuggestionSelected: (suggestion) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => ProductPage(product: suggestion)
+    ));
+  },
+)*/
