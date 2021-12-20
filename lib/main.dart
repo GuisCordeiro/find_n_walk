c5236712953c9abedd5931b4727ab074066c710a
Eduardo Antunes <eduardoantunes986@gmail.com>
2021-12-20 11:18:54 -0300

    Conectei o app ao projeto Firebase e o inicializei no código

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
-import 'package:findnwalk/pages/index.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:findnwalk/pages/index.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
-
-
